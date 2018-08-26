f9e1516ac441a97ce61ed879097e54c8beff1155
bdhagger <38987202+bdhagger@users.noreply.github.com>
2018-08-25 18:05:17 -0700

    start over

--- Lab6.asm
+++ Lab6.asm
@@ -16,60 +16,70 @@
 
 # REGISTER USAGE
 
-# $a0: pitch
-# $a1: duration in milliseconds
+#---------- play_song ----------
 
-# $t0: holds string 
-# $t1: holds character 
-# $t2: holds zero for null character
+# input: $a0 - address of first character in string containing song
+#        $a1 - tempo of song in beats per minute
 
-# $v0: sets type of output for syscall
+#------- get_song_length -------
+
+# input:  $a0 - address of first character in string containing song
+# output: $v0 - number of notes in song
+
+#---------- play_note ----------
+
+# input: $a0 - pitch
+#        $a1 - note duration in milliseconds
+
+#---------- read_note ----------
+
+# input: $a0 - address of first character in string containing note encoding
+#        $a1 - rhythm of previous note
+#
+# output: $v0 - note rhythm in bits [31:16], note pitch in bits [15:0]
+#               note rhythm: (1 = 4 beats, 2 = 2 beats, 4 = 1 beat,
+#                             8 = 1/2 beat, 16 = 1/4 beat)
+#               note pitch: (MIDI value, 0-127)
+#         $v1 - address of first character of what would be next note
+
+#---------- get_pitch ----------
+
+# input: $a0 - address of first character in string containing note encoding
+#
+# output: $v0 - MIDI pitch value
+#         $v1 - address of character after pitch is determined
+
+#--------- get_rhythm -----------
+
+# input: $a0 - address of character in string containing song encoding
+#              after pitch is determined
+#        $a1 - previous note rhythm
+#
+# output: $v0 - note rhythm, default to previous note rhythm if no number
+#               is present in note encoding
+#  
+#         $v1 - address of first character of next note
+
+# $t0 holds the string
 
 ####################################################################################
 
 .data
        song:   .asciiz "e'8 g' e'' c'' d'' g''"
-       space:  .asciiz " "
-       sp:     .asciiz "\nits a space\n"
-
+       
 .text
        la      $t0 song      # put song string in t0
-       li      $t3 0x20      
-       li      $t2 0         # initialize t2 to null
-       li      $a3 127
+       li      $t3 0x20      # set $t3 as an ascii space
+       li      $a3 127       # set volume to 127
+       
+       jal play_song
+       
+       jal exit
        
 play_song:
-
-       lb      $t1 ($t0)     # put character in t1
-       beq     $t1 $t2  exit # check if reached the end of the string
-       beq     $t1 $t3  play_note  # check if it's a space
-
-
-       move    $a0  $t1      # get character to print
-       li      $v0  11       # set syscall to print the character
+      
+       
+exit:
+       li $v0, 10
        syscall
        
- inc:   
-       add     $t0  $t0  1   # increment loop
-       j play_song
-       
-play_note:
-
-       li $a0 65
-       li $a1 1000
-
-
-       li $v0 33
-       syscall
-       j inc
-
-exit:  
-       li $a0 65
-       li $a1 1000
-
-
-       li $v0 33
-       syscall
-       
-       li      $v0  10        # set syscall exit the program
-       syscall
